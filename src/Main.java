eb1bf399772ffab71e4634bdebaea82ab117a9e3
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2023-08-16 16:25:06 +0300

    add: generate gray color and shade

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,72 @@ public class Main {
         scanner.close();
 
         // Generate color shades
-        System.out.println("Generating color shades...");
         List<String> shades = generateColorShades(line);
+        String grayscale = convertToGrayscale(line);
+        List<String> grayscaleShades = generateColorShades(grayscale);
+
+
+        // Print results
+        // Color shades
+        System.out.println("Generating color shades...");
         for (String shade : shades) {
             System.out.println(shade);
         }
         System.out.println("Done!");
+        System.out.println();
+
+        // Grayscale
+        System.out.println("Converting to grayscale...");
+        System.out.println(grayscale);
+        System.out.println("Done!");
+        System.out.println();
+
+        // Grayscale shades
+        System.out.println("Generating grayscale shades...");
+        for (String shade : grayscaleShades) {
+            System.out.println(shade);
+        }
+        System.out.println("Done!");
+        System.out.println();
 
     }
+
+    public static String convertToGrayscale(String hexColor) {
+        return convertToGrayscale(hexColor, .925);
+    }
+
+    /**
+     * Creates a grayed version of the given hex color.
+     *
+     * @param hexColor The hex color value, with or without the '#' symbol.
+     * @param factor   The factor to gray the color by, ranging from 0 (original color) to 1 (completely gray).
+     * @return The grayed version of the hex color as a string, including the '#' symbol.
+     */
+    public static String convertToGrayscale(String hexColor, double factor) {
+        if (hexColor.startsWith("#")) {
+            hexColor = hexColor.substring(1);
+        }
+
+        int red = Integer.parseInt(hexColor.substring(0, 2), 16);
+        int green = Integer.parseInt(hexColor.substring(2, 4), 16);
+        int blue = Integer.parseInt(hexColor.substring(4, 6), 16);
+
+        // Calculating the gray value using a common weighted method
+        int grayValue = (int) (0.299 * red + 0.587 * green + 0.114 * blue);
+
+        // Blending the original color with the gray value using the provided factor
+        red = (int) (red * (1 - factor) + grayValue * factor);
+        green = (int) (green * (1 - factor) + grayValue * factor);
+        blue = (int) (blue * (1 - factor) + grayValue * factor);
+
+        return String.format("#%02x%02x%02x", red, green, blue);
+    }
+
 
     /**
      * Takes a given hex color, creates 4 lighter and 4 darker versions of the color.
      * The colors are saved in a list from light to dark, with the original color in the center.
+     *
      * @param hexColor Input hex color value.
      * @return A list of 9 elements, the first 4 being lighter, the fifth the original color, and the last 4 darker versions.
      */
@@ -53,8 +107,9 @@ public class Main {
 
     /**
      * Adjusts a given hex color by the specified factor, either making it lighter or darker.
-     * @param color The hex color value without the '#' symbol.
-     * @param factor The factor by which to adjust the color. Ranges from 0 to 1, with higher values making a more drastic change.
+     *
+     * @param color   The hex color value without the '#' symbol.
+     * @param factor  The factor by which to adjust the color. Ranges from 0 to 1, with higher values making a more drastic change.
      * @param lighter If true, makes the color lighter; if false, makes it darker.
      * @return The adjusted hex color as a string, including the '#' symbol.
      */
@@ -64,20 +119,16 @@ public class Main {
         int blue = Integer.parseInt(color.substring(4, 6), 16);
 
         if (lighter) {
-            red = Math.min(red + (int)((255 - red) * factor), 255);
-            green = Math.min(green + (int)((255 - green) * factor), 255);
-            blue = Math.min(blue + (int)((255 - blue) * factor), 255);
+            red = Math.min(red + (int) ((255 - red) * factor), 255);
+            green = Math.min(green + (int) ((255 - green) * factor), 255);
+            blue = Math.min(blue + (int) ((255 - blue) * factor), 255);
         } else {
-            red = Math.max(red - (int)(red * factor), 0);
-            green = Math.max(green - (int)(green * factor), 0);
-            blue = Math.max(blue - (int)(blue * factor), 0);
+            red = Math.max(red - (int) (red * factor), 0);
+            green = Math.max(green - (int) (green * factor), 0);
+            blue = Math.max(blue - (int) (blue * factor), 0);
         }
 
         return String.format("#%02x%02x%02x", red, green, blue);
     }
-
-
-
-
 
 }
