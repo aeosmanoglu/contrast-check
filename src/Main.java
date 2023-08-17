a7bb4aa98a66ea739da7affa22e7638fc2bde2b0
Abuzer Emre Osmanoğlu <aeosmanoglu@gmail.com>
2023-08-17 15:13:22 +0300

    add: csv file output

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,29 +19,11 @@ public class Main {
         String grayscale = convertToGrayscale(line);
         List<String> grayscaleShades = generateColorShades(grayscale);
 
-
-        // Print results
-        // Color shades
+        // Compare color shades to grayscale shades
         System.out.println("Generating color shades...");
-        for (String shade : shades) {
-            System.out.println(shade);
-        }
-        System.out.println("Done!");
-        System.out.println();
-
-        // Grayscale
-        System.out.println("Converting to grayscale...");
-        System.out.println(grayscale);
-        System.out.println("Done!");
-        System.out.println();
-
-        // Grayscale shades
-        System.out.println("Generating grayscale shades...");
-        for (String shade : grayscaleShades) {
-            System.out.println(shade);
-        }
-        System.out.println("Done!");
-        System.out.println();
+        String csvFileName = removeHex(line) + "-contrast-ratios.csv";
+        compareColorListsToCSV(shades, grayscaleShades, csvFileName);
+        System.out.println(csvFileName + " created!");
 
     }
 
@@ -54,9 +39,7 @@ public class Main {
      * @return The grayed version of the hex color as a string, including the '#' symbol.
      */
     public static String convertToGrayscale(String hexColor, double factor) {
-        if (hexColor.startsWith("#")) {
-            hexColor = hexColor.substring(1);
-        }
+        hexColor = removeHex(hexColor);
 
         int red = Integer.parseInt(hexColor.substring(0, 2), 16);
         int green = Integer.parseInt(hexColor.substring(2, 4), 16);
@@ -82,9 +65,7 @@ public class Main {
      * @return A list of 9 elements, the first 4 being lighter, the fifth the original color, and the last 4 darker versions.
      */
     public static List<String> generateColorShades(String hexColor) {
-        if (hexColor.startsWith("#")) {
-            hexColor = hexColor.substring(1);
-        }
+        hexColor = removeHex(hexColor);
 
         List<String> shades = new ArrayList<>();
 
@@ -130,5 +111,83 @@ public class Main {
 
         return String.format("#%02x%02x%02x", red, green, blue);
     }
+
+
+    /**
+     * Compares two lists of hex color strings, calculates the contrast ratio between each pair of corresponding colors
+     * according to W3C standards, and writes the results as comments in a given CSS file.
+     * The contrast ratios are determined based on the relative luminance of the colors.
+     *
+     * @param list1       The first list of hex color strings, with or without the '#' symbol at the start.
+     * @param list2       The second list of hex color strings, with or without the '#' symbol at the start.
+     * @param csvFileName The name of the CSS file where the contrast ratios will be written as comments.
+     */
+    public static void compareColorListsToCSV(List<String> list1, List<String> list2, String csvFileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFileName))) {
+            writer.write("Name, Hex Code, Grayscale Hex Code, Contrast Ratio\n");
+            for (int i = 0; i < list1.size(); i++) {
+                String color1 = list1.get(i);
+                for (String color2 : list2) {
+                    double contrast = calculateContrast(color1, color2);
+                    writer.write("Primary-" + (i + 1) * 100 + ", " + color1 + ", " + color2 + ", " + contrast + "\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    /**
+     * Calculates the contrast ratio between two given hex color strings.
+     * The contrast ratio is determined according to the W3C recommendations
+     * and is based on the relative luminance of the colors. It's useful in
+     * determining the legibility of text and the separation of visual elements.
+     *
+     * @param color1 The first hex color string, with or without the '#' symbol at the start.
+     * @param color2 The second hex color string, with or without the '#' symbol at the start.
+     * @return The contrast ratio between the two colors, a value typically in the range of 1 (no contrast) to 21 (maximum contrast).
+     */
+    public static double calculateContrast(String color1, String color2) {
+        double l1 = calculateLuminance(color1);
+        double l2 = calculateLuminance(color2);
+
+        return (Math.max(l1, l2) + 0.05) / (Math.min(l1, l2) + 0.05);
+    }
+
+
+    /**
+     * Calculates the relative luminance of a given hex color according to the sRGB color space.
+     * The calculated luminance is based on human perception and follows the W3C recommendations.
+     * It considers the gamma correction and weights the color channels according to their contribution to perceived brightness.
+     *
+     * @param color The hex color string, with or without the '#' symbol at the start.
+     * @return The relative luminance of the color, a value between 0 (black) and 1 (white).
+     */
+    private static double calculateLuminance(String color) {
+        color = removeHex(color);
+
+        double red = Integer.parseInt(color.substring(0, 2), 16) / 255.0;
+        double green = Integer.parseInt(color.substring(2, 4), 16) / 255.0;
+        double blue = Integer.parseInt(color.substring(4, 6), 16) / 255.0;
+
+        red = (red <= 0.03928) ? red / 12.92 : Math.pow((red + 0.055) / 1.055, 2.4);
+        green = (green <= 0.03928) ? green / 12.92 : Math.pow((green + 0.055) / 1.055, 2.4);
+        blue = (blue <= 0.03928) ? blue / 12.92 : Math.pow((blue + 0.055) / 1.055, 2.4);
+
+        return 0.2126 * red + 0.7152 * green + 0.0722 * blue;
+    }
+
+    /**
+     * Removes the hash symbol ('#') from the beginning of the given hex color string if it exists.
+     *
+     * @param color The hex color string, with or without the '#' symbol at the start.
+     * @return The hex color string without the '#' symbol at the start.
+     */
+    private static String removeHex(String color) {
+        return color.startsWith("#") ? color.substring(1) : color;
+    }
+
 
 }
